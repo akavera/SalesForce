a9405d525973adced9967459e8c69389db233688
akavera <97555970+akavera@users.noreply.github.com>
2022-02-22 20:52:29 +0300

    Essentials

---
+++ src/test/java/staticdata/WebTimeouts.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class WebTimeouts {
+    public static int PAGE_LOAD_TIMEOUT = 20;
+    public static int SCRIPT_TIMEOUT = 20;
+    public static int IMPLICIT_TIMEOUT = 20;
+}
---
+++ src/test/java/staticdata/WebUrls.java
@@ -0,0 +1,5 @@
+package pages;
+
+public class WebUrls {
+    public static final String SALESFORCE_URL="https://tms7.my.salesforce.com/";
+}
